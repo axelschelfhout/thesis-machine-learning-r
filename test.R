9fe7449b9f58946c7d0ce42a5dc137bb1b29aca8
Axel Schelfhout <axelschelfhout@gmail.com>
2016-05-23 10:41:09 +0200

    Found better ways to cluster and added knn classification models to test

--- test.R
+++ test.R
@@ -4,36 +4,105 @@ require(bit64)
 #data = read.csv('Projects/ml-r/fakenames50k.csv')
 data = fread('Projects/ml-r/fakenames50k.csv')
 
-#   
 ages = as.numeric(data$Age)
-genders = data$Gender
 kilos = as.numeric(data$Kilograms)
 lengths = as.numeric(data$Centimeters)
+pounds = as.numeric(data$Pounds)
 states = data$StateFull
 bloodtype = data$BloodType
 
+# Map Genders Character values to Numeric
+genders_as_string = data$Gender
+data[data$Gender == 'female',] <- 1
+data[data$Gender == 'male',] <- 0
+genders_as_int = as.numeric(data$Gender)
+
+genders = genders_as_string # String used in classification, int used in clustering
+
 # 
-fullSet = data.frame(list(ages,genders,kilos,lengths,states,bloodtype))
-colnames(fullSet) <- c('Age','Gender','Kilo','Length','State','Bloodtype')
+#fullSet = data.frame(list(ages,kilos,pounds,lengths,genders,states,bloodtype))
+#colnames(fullSet) <- c('Age','Kilo','Pounds','Length','Gender','State','Bloodtype')
+fullSet = data.frame(list(ages,kilos,pounds,lengths,genders))
+colnames(fullSet) <- c('Age','Kilo','Pounds','Length','Gender')
 
 #
 threshold = round(nrow(fullSet)*0.7)
 train = fullSet[1:threshold, ]
 evaluate = fullSet[(threshold+1):nrow(fullSet), ]
 
-# K-Means Clustering
-# We can't use the String. So for this test, dont use the gender collumn
+#
+#k <- kmeans(fullSet, 5)
+#plot(k$centers)
+#
+
+# Test the ggvis library
+#library(ggvis)
+#forggvis = fullSet[1:round(nrow(fullSet)*0.05),]
+#forggvis %>% ggvis(~Kilo, ~Length, fill = ~Gender) %>% layer_points()
+
+
+
+# KNN Classification
+library(class)
+set.seed(1234)
+
+knnSet <- copy(fullSet)
+threshold = round(nrow(knnSet)*0.7)
+knnSet.training <- knnSet[1:threshold, 1:4]
+knnSet.test <- knnSet[(threshold+1):nrow(knnSet), 1:4]
+
+knnSet.trainLabels <- knnSet[1:threshold, 5]
+knnSet.testLabels <- knnSet[(threshold+1):nrow(knnSet), 5]
+
+prediction <- knn(train = knnSet.training, test = knnSet.test, cl = knnSet.trainLabels, k=2)
+
+library(gmodels)
+CrossTable(x = knnSet.testLabels, y = prediction, prop.chisq = FALSE)
+
+#
+# https://www.datacamp.com/community/tutorials/machine-learning-in-r
+#
+
+# -- # # -- # Logistic Regression Classification # -- # # -- # 
+lrSet = copy(fullSet)
+
+# Multinomial logistic regression
+library(VGAM)
+mlrFit <- vglm(Gender~., family=multinomial, data=lrSet)
+summary(mlrFit)
+
+mlrProbabilities <- predict(mlrFit, lrSet[,1:4], type="response")
+mlrPredictions <- apply(mlrProbabilities, 1, which.max)
+mlrPredictions[which(mlrPredictions=="1")] <- levels(lrSet$Gender)[1]
+mlrPredictions[which(mlrPredictions=="2")] <- levels(lrSet$Gender)[2]
+table(mlrPredictions, lrSet$Gender)
+
+#Linear Discriminant Analysis
+#LDA is a classification method that finds a linear combination of data attributes that best separate the data into classes.
+library(MASS)
+ldaFit <- lda(Gender~., data=lrSet)
+#summary(ldaFit)
+ldaPredictions <- predict(ldaFit, lrSet[,1:4])$class
+table(ldaPredictions, lrSet$Gender)
+#
+
+
+# -- # # -- # K-Means Clustering # -- # # -- #
+
+# Determine the amount of clusters
 ctrain = train[,-1]
 wss <- (nrow(ctrain)-1)*sum(apply(ctrain,2,var))
-
 for(i in 2:15) wss[i] <- sum(kmeans(ctrain, centers=i)$withinss)
 plot(1:15, wss, type='b', xlab="Number of clusters", ylab="Within groups sum of squares")
 
+#
 # FIT IT
-fit <- kmeans(ctrain, 4)
+fit <- kmeans(ctrain, 5)
 aggregate(ctrain, by=list(fit$cluster), FUN=mean)
-ctrain <- data.frame(ctrain, fit$cluster, train$Geslacht)
+ctrain <- data.frame(ctrain, fit$cluster, train$Gender)
 View(ctrain)
+
+
 
 #### **** #### #### **** #### #### **** #### #### **** #### #### **** ####
 
